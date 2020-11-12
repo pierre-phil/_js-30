247dcf09cce3433504e68e8b105b65c3ddb76245
pierre-phil <pierre-philippon@live.fr>
2020-11-12 19:56:30 +0100

    done

---
+++ js-30/6-ajax type ahead/index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+
+fetch(endpoint)
+  .then((blob) => blob.json())
+  // use spread method to prevent having data array into bob array
+  .then((data) => cities.push(...data));
+
+console.log("cities", cities);
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter((el) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return el.city.match(regex) || el.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  console.log(matchArray);
+  const html = matchArray
+    .map((el) => {
+      const regex = new RegExp(this.value, "gi");
+      const cityName = el.city.replace(
+        regex,
+        `<span class="h1">${this.value}</span>`
+      );
+      const stateName = el.state.replace(
+        regex,
+        `<span class="h1">${this.value}</span>`
+      );
+      return `<li>
+    <span class="name">${cityName}, ${stateName}</span>
+    <span class="population">${numberWithCommas(el.population)}</span>
+    </li>`;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
