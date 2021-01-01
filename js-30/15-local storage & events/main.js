8258f42ccbcd4ad8455adc56fc662e67105ce3f6
pierre-phil <pierre-philippon@live.fr>
2021-01-01 11:29:17 +0100

    15

--- js-30/15-local storage & events/main.js
+++ js-30/15-local storage & events/main.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function addItem(e) {
   e.preventDefault();
@@ -13,6 +13,7 @@ function addItem(e) {
   items.push(item);
   console.log("item :", item);
   populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
   console.log("items :", items);
   this.reset();
 }
@@ -25,10 +26,27 @@ function populateList(plates = [], platesList) {
     <input type='checkbox' data-index=${index} id="item${index}" ${
         el.done ? "checked" : ""
       }/>
-    <label for="item${index}">${el.text}</label>
+      <label for="item${index}">${el.text}</label>
     </li>`;
     })
     .join("");
 }
 
+function toggleDone(e) {
+  if (!e.target.matches("input")) return;
+  // skip the rest of the function if what we clicked is not an input
+  console.log(e.target);
+  const el = e.target;
+  const index = el.dataset.index;
+  console.log("index :", index);
+  // these are the indexs that we defined in populateList map
+  items[index].done = !items[index].done;
+  // then if 'done' value for an element index is false, put it to true and vice-versa
+  // then update localStorage
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
 addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
+
+populateList(items, itemsList);
